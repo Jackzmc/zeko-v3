989ddc9bf35a8cfa0c0722b33bf683a996105509
Jackz <me@jackz.me>
2020-07-19 18:14:19 -0500

    Added client.utils

--- src/core/Functions.js
+++ src/core/Functions.js
@@ -1,9 +1,14 @@
 import path from 'path'
 import { fileURLToPath } from 'url'
+import Utils from '../Utils.js'
+
 const moduleURL = new URL(import.meta.url);
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default function(client) {
     client.PREFIX = process.env.BOT_PREFIX || ">"
     client.ROOT_DIR = path.resolve(__dirname,"../../"); 
     client.managers = {}
+
+    client.utils = Utils(client);
 }
