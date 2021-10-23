7f68d8abd0307a79b505dbd127b101e5e3ddbb77
Jackz <me@jackz.me>
2021-10-22 22:05:09 -0500

    Switch messageCreate response to .reply()

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -3,10 +3,11 @@ import getopts, { ParsedOptions } from 'getopts'
 import { CommandFlag, CommandFlagOptions, FlagType } from '../types/Command.js'
 import { Client, Message } from 'discord.js';
 import Logger from '../Logger.js'
-import { RegisteredCommand } from '../managers/CommandManager.js';
-import ModuleManager from '../managers/ModuleManager.js';
-import HelpModule from '../modules/help.js';
+import { RegisteredLegacyCommand } from '../managers/CommandManager.js';
+import CommandManager from '../managers/CommandManager.js';
 import { Intents } from 'discord.js'
+
+import HelpCommand from '../commands/help.js'
 
 export const INTENTS = Intents.FLAGS.GUILD_MESSAGES | Intents.FLAGS.DIRECT_MESSAGES
 
@@ -16,18 +17,14 @@ export default class extends CoreEvent {
     constructor(client: Client, logger: Logger) {
         super(client, logger);
         this.#cmdManager = client.managers.commandManager;
-        const helpModule = ModuleManager.getInstance().getCoreModule("help", true) as HelpModule
-        if(helpModule) {
-            this.#generateHelpCommand = helpModule.generateHelpCommand
-        }else{
-            logger.warn('Could not find help module, help method disabled.');
-        }
+        this.#generateHelpCommand = (CommandManager.getInstance().getSlashCommand('help', true).command as HelpCommand).generateHelpCommand
     }
     config() {
         return {
         }
     }
     every(msg: Message): Promise<boolean> {
+        this.logger.debug('msg')
         return new Promise((resolve) => {
             if(msg.author.bot) return resolve(true); //Ignore bots.
             if(msg.content.startsWith(this.client.PREFIX)) {
@@ -36,10 +33,10 @@ export default class extends CoreEvent {
                 
                 if(/\s/.test(this.client.PREFIX)) args.shift(); //shift if prefix has space
                 const command_name: string = /\s/.test(this.client.PREFIX) ? args.shift().toLowerCase() : args.shift().slice(this.client.PREFIX.length).toLowerCase();
-                const cmd: RegisteredCommand = this.#cmdManager.getCommand(command_name, true)
+                const cmd: RegisteredLegacyCommand = this.#cmdManager.getCommand(command_name, true)
                 if(cmd) {
                     if(cmd.config.guildOnly && msg.channel.type === "DM") {
-                        return msg.channel.send('This commanad only works in guilds.')
+                        return msg.reply('This commanad only works in guilds.')
                     }
                     try {
                         //parse arguments with getopts package (--flag)
@@ -79,21 +76,21 @@ export default class extends CoreEvent {
                         if(options.help || (cmd.config.usageIfNotSet && newArgs.length == 0)) {
                             //const help = cmdManager.getCommand('help').generateHelpCommand(client,cmd);
                             if(this.#generateHelpCommand) {
-                                return msg.channel.send(this.#generateHelpCommand(cmd))
+                                return msg.reply(this.#generateHelpCommand(cmd))
                             }else{
-                                return msg.channel.send("Could not print help command at this time.")
+                                return msg.reply("Could not print help command at this time.")
                             }
                         }
                         Promise.resolve(cmd.command.run(msg, newArgs, flags))
                         .then(() => resolve(true))
                         .catch(err => {
                             this.logger.warn(`Command ${cmd.name} had an error:`, err.message)
-                            msg.channel.send('**Command Execution Error**\n`' + err.message + "`")
+                            msg.reply('**Command Execution Error**\n`' + err.message + "`")
                             .catch(() => {})
                             resolve(false)
                         })
                     }catch(err) {
-                        msg.channel.send('**Command Error**\n`' + err.message + "`")
+                        msg.reply('**Command Error**\n`' + err.message + "`")
                         .catch(err => this.logger.warn("Failed to send command error message",err.message))
                         this.logger.warn(`Command ${cmd.name} had an error:\n    `, err.stack)
                         resolve(false)
