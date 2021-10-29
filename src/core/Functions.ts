6496aab3df7ab59b1aac61af0c61f8e7c7eabbf5
Jackz <me@jackz.me>
2021-10-29 14:46:50 -0500

    Remove settings manager from functions

--- src/core/Functions.ts
+++ src/core/Functions.ts
@@ -8,7 +8,6 @@ import Manager from '../managers/Manager.js';
 import CommandManager from '../managers/CommandManager';
 import ModuleManager from '../managers/ModuleManager';
 import EventManager from '../managers/EventManager';
-import SettingsManager from '../managers/SettingsManager.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -16,7 +15,6 @@ export interface Managers {
     commandManager: CommandManager,
     moduleManager: ModuleManager,
     eventManager: EventManager,
-    settingsManager: SettingsManager
 }
 
 export default function(client: Client) {
@@ -26,7 +24,6 @@ export default function(client: Client) {
         commandManager: null,
         moduleManager: null,
         eventManager: null,
-        settingsManager: null
     } as Managers
     client.utils = Utils(client);
     client.evns = {}
