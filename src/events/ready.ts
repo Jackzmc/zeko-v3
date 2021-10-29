560c541853c345d532fd646a683fd4f270433496
Jackz <me@jackz.me>
2021-10-29 12:14:47 -0500

    Log when full bot is ready

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,17 +2,19 @@ import CoreEvent from '../core/types/CoreEvent.js'
 export default class extends CoreEvent {
     #fired = false
 
-    every(/* args */) {
+    async every(/* args */) {
+        this.logger.info(`Discord.js now ready`);
         if(!this.#fired) {
             // TODO: Delay until this.core.isReady to prevent race-condition
             this.#fired = true;
-            Promise.all([
+            await Promise.all([
                 this.core.modules.ready(),
                 this.core.commands.ready(),
                 this.core.events.ready()
             ])
+            this.logger.info(`Bot now ready`);
         }
+        return true
         //Fires every time.
-        this.logger.info(`Bot now ready`);
     }
 }
