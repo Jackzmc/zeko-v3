0c17e93db0e5d9e8675436642c5080a1015aa63d
Jackz <me@jackz.me>
2022-02-26 21:33:34 -0600

    Fix autocomplete events not firing

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -18,7 +18,6 @@ export default class extends CoreEvent {
 
     async every(interaction: Interaction) {
         if(interaction.isButton()) {
-            this.logger.debug(`btn press -> ${interaction.id}`)
             if(this.buttonManager.onInteract(interaction)) {
                 return false
             }
@@ -27,59 +26,58 @@ export default class extends CoreEvent {
             if(this.selectManager.onInteract(interaction, interaction.values)) {
                 return false
             }
-        }
-        if (!interaction.isCommand()) return;
-        
+        } else if(interaction.isAutocomplete() || interaction.isCommand()) {
 
-        const slash = this.core.commands.getSlashCommand(interaction.commandName)
-        if(!slash) return;
+            const slash = this.core.commands.getSlashCommand(interaction.commandName)
+            if(!slash) return;
 
-        let options: OptionResult = new OptionResult(interaction.options, slash.data.options)
-
-        if(interaction.isAutocomplete()) { 
-            const autocomplete: AutocompleteInteraction = interaction //Fix being set to 'never
-            const focused = autocomplete.options.getFocused(true)
-            const handler = slash.handlers.autocomplete[options.subcommand]
-            try {
-                if(handler) {
-                    await handler(autocomplete, focused)
-                } else {
-                    await slash.command.onAutocomplete(autocomplete, focused)
-                }
-            } catch(err) {
-                this.logger.error(`[cmd/${slash.data.name}] autocomplete threw an error`, err)
-            }
-            return
-        }
-
-        try {
-            const handler = slash.handlers.default[options.subcommand]
-            try {
-                if(handler)
-                    await handler(interaction, options)
-                else
-                    await slash.command.run(interaction, options)
-            } catch(err) {
-                const msg = '**Command Error**\n`' + err.message + "`"
+            let options: OptionResult = new OptionResult(interaction.options, slash.data.options)
+            if(interaction.isAutocomplete()) { 
+                const autocomplete: AutocompleteInteraction = interaction //Fix being set to 'never
+                const focused = autocomplete.options.getFocused(true)
+                const handler = slash.handlers.autocomplete[options.subcommand]
                 try {
-                    if(interaction.replied)
-                        await interaction.editReply(msg)
-                    else if(interaction.deferred) 
-                        await interaction.followUp(msg)
-                    else
-                        await interaction.reply(msg)
+                    if(handler) {
+                        await handler(autocomplete, focused)
+                    } else {
+                        await slash.command.onAutocomplete(autocomplete, focused)
+                    }
                 } catch(err) {
-                    this.logger.warn("Failed to send command error message",err.message)
+                    this.logger.error(`[cmd/${slash.data.name}] autocomplete threw an error`, err)
                 }
-                this.logger.warn(`Command ${slash.data.name} had an error:\n    `, err.stack)
-                return false
+                return
             }
-        } catch(err) {
-            this.logger.warn(`Command ${slash.data.name} experienced option parsing error:\n    `, err.stack)
-            const msg = `**Internal Error**\n${err.message}`
-            if(interaction.replied) await interaction.editReply(msg) 
-            else await interaction.reply(msg)
+
+            if (!interaction.isCommand()) return;
+            try {
+                const handler = slash.handlers.default[options.subcommand]
+                try {
+                    if(handler)
+                        await handler(interaction, options)
+                    else
+                        await slash.command.run(interaction, options)
+                } catch(err) {
+                    const msg = '**Command Error**\n`' + err.message + "`"
+                    try {
+                        if(interaction.replied)
+                            await interaction.editReply(msg)
+                        else if(interaction.deferred) 
+                            await interaction.followUp(msg)
+                        else
+                            await interaction.reply(msg)
+                    } catch(err) {
+                        this.logger.warn("Failed to send command error message",err.message)
+                    }
+                    this.logger.warn(`Command ${slash.data.name} had an error:\n    `, err.stack)
+                    return false
+                }
+            } catch(err) {
+                this.logger.warn(`Command ${slash.data.name} experienced option parsing error:\n    `, err.stack)
+                const msg = `**Internal Error**\n${err.message}`
+                if(interaction.replied) await interaction.editReply(msg) 
+                else await interaction.reply(msg)
+            }
+            return true
         }
-        return true
     }
 }
