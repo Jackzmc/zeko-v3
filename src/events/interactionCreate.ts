996c44f82ea3da23fa165dad443b2e239b476dcb
Jackz <jacksonbixby@gmail.com>
2022-07-03 21:45:25 -0500

    Actually actually fix handler

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -36,7 +36,7 @@ export default class extends CoreEvent {
                 const handler = slash.handlers.autocomplete[focused.name]
                 try {
                     if(handler) {
-                        await handler.bind(slash)(autocomplete, focused)
+                        await handler.bind(slash.command)(autocomplete, focused)
                     } else {
                         await slash.command.onAutocomplete(autocomplete, focused)
                     }
@@ -51,7 +51,7 @@ export default class extends CoreEvent {
                 const handler = slash.handlers.default[options.subcommand]
                 try {
                     if(handler)
-                        await handler.bind(slash)(interaction, options)
+                        await handler.bind(slash.command)(interaction, options)
                     else
                         await slash.command.run(interaction, options)
                 } catch(err) {
